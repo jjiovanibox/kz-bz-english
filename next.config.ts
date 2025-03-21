e39764a8c30c744e593d3b440a6978daff621281
jjiovanibox <jjiovanibox@gmail.com>
2025-03-21 06:22:37 +0000

    added base pages

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // カスタム Webpack 設定をここに追加
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-      };
-    }
-
-    // 例: 新しいローダーを追加
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+
+
+module.exports = {
+  images: {
+    domains: ['res.cloudinary.com'], // Cloudinaryのホスト名を追加
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/my-account/**",
+        search: "",
+      },
+    ],
+  },
+};
